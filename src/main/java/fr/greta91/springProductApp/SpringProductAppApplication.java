59d01ec65bf467ffd4bed106e73d0d1fa8790dbf
soupramanien <soupramani@yahoo.fr>
2022-02-21 15:59:16 +0100

    initial commit

---
+++ src/main/java/fr/greta91/springProductApp/SpringProductAppApplication.java
@@ -0,0 +1,13 @@
+package fr.greta91.springProductApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/greta91/springProductApp/SpringProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.greta91.springProductApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8b24f74bec1a428745ddbe2c507f884b3c73c4f2
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-02-22 13:48:17 +0100

    Add Delete and Update methods

--- src/main/java/fr/greta91/springProductApp/controller/ProductController.java
+++ src/main/java/fr/greta91/springProductApp/controller/ProductController.java
@@ -35,61 +35,71 @@ import fr.greta91.springProductApp.repository.ProductRepository;
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
-	
-	 @Autowired
-	 ProductRepository productRepository;
-	 
-	 @RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	 public Collection<Product> products() {
-		 return productRepository.findAll();
-	 }
-	 
-	 @RequestMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	 public ResponseEntity<Product> products(@PathVariable long id) {
-		 Optional<Product> found = productRepository.findById(id);
-		 return found.map(p->ResponseEntity.ok(p)).orElse(ResponseEntity.notFound().build());
-			
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Product> products() {
+		return productRepository.findAll();
+	}
+
+	@RequestMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Product> products(@PathVariable long id) {
+		Optional<Product> found = productRepository.findById(id);
+		return found.map(p->ResponseEntity.ok(p)).orElse(ResponseEntity.notFound().build());
+
 		/* est egal à:
 		 * 
 		 * Optional<Product> found = productRepository.findById(id);
 		 * if(found.isPresent()) {
 		 *		return ResponseEntity.ok(found.get());
-	 	 * }
-	 	 * return ResponseEntity.notFound().build();
-	 	 * 
-	 	 */
-	 }
-	 
-	 
-	 /*
-	  * use Postman to try out insertion
-	  */
-	 @RequestMapping(method = RequestMethod.POST)
-	 public ResponseEntity<ProductDto> delete(@RequestBody ProductDto productDto){
-		 try {
-			 Product p = new Product(productDto);
-			 productRepository.save(p);
-			 productDto.setId(p.getId());
-			 return ResponseEntity.ok(productDto);
-			
+		 * }
+		 * return ResponseEntity.notFound().build();
+		 * 
+		 */
+	}
+
+
+	/*
+	 * use Postman to try out insertion
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<ProductDto> save(@RequestBody ProductDto productDto){
+		try {
+			Product p = new Product(productDto);
+			productRepository.save(p);
+			productDto.setId(p.getId());
+			return ResponseEntity.ok(productDto);
+
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
 		}
 
-	 }
-	 
-	 @RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
-	 public ResponseEntity<Product> delete(@PathVariable long id) {
-		 try {
-			 Product p = new Product(productDto);
-			 productRepository.save(p);
-			 productDto.setId(p.getId());
-			 return ResponseEntity.ok(productDto);
-			
+	}
+
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Boolean> delete(@PathVariable long id) {
+		try {
+			productRepository.deleteById(id);
+			return ResponseEntity.ok(true);
+
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
 		}
 
-	 }
+	}
 
+	@RequestMapping(method=RequestMethod.PUT)
+	public ResponseEntity<ProductDto> update(@RequestBody ProductDto productDto) {
+		try {
+			Product p = new Product(productDto);
+			productRepository.save(p);
+			productDto.setId(p.getId());
+			return ResponseEntity.ok(productDto);
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 }
